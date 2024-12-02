2616fbb08003c0e679593529c2439e0b303784fd
Diksha-av <dikshagurupawar@gmail.com>
2024-12-02 23:56:58 +0100

    created catalogue.sh and catalogue.service

---
+++ catalogue.sh
@@ -0,0 +1,38 @@
+#this is file for catalogue server
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+#cp catalogue.service /etc/systemd/system/catalogue.service
+
+#systemctl daemon-reload
+
+#systemctl enable catalogue
+#systemctl restart catalogue
+
+#cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y
+
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+systemctl restart catalogue
